ccc4782064fcfe44613c06a422db7c82ff95d8f4
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-04 03:12:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
   end
   
   def match(array_of_words)
-    array_of_words.select do |world|
-      (@word.split("").sort) == (world.split("").sort)
+    array_of_words.select do |word|
+      (@word.split("").sort) == (word.split("").sort)
     end
   end
 end
